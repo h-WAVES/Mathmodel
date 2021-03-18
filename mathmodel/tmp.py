661427446f3d66dcf90238c84b4bbe83735623e6
黄海涛 <huanghaitao02@meituan.com>
2021-03-18 17:06:52 +0800

    m

--- mathmodel/tmp.py
+++ mathmodel/tmp.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*- 
-
-# @Time : 2021-03-18 13:49 
-# @Author : hht 
-# @File : tmp.py
-# @Desc:temp file
 
 
-from mathmodel import dataset
 
-boston_data=dataset.load_boston(type='pandas')
-print(type(boston_data))
+
