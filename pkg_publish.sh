3de81820e2353527d577ed98210430a78b55597d
黄海涛 <huanghaitao02@meituan.com>
2021-03-19 10:55:25 +0800

    m

--- pkg_publish.sh
+++ pkg_publish.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-rm -rf dist/* && python3 setup.py sdist && twine upload dist/*
-#rm -rf dist/* && python3 setup.py sdist && python setup.py bdist_wheel --universal && twine upload dist/*
+rm -rf dist/* && python3 setup.py sdist && python setup.py bdist_wheel --universal && twine upload dist/*
