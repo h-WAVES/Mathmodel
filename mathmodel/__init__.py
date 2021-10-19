8071fd4fe84268e0199a13558647a1b77847804f
huanghaitao <huanghaitao@lixiang.com>
2021-10-19 16:15:28 +0800

    opt py

--- mathmodel/__init__.py
+++ mathmodel/__init__.py
@@ -3,7 +3,7 @@ from .dataset import *
 from .quant import *
 from .statisticmodel import *
 from  .utils import *
-__version__ = '0.1.22'
+__version__ = '0.1.23'
 check_version(__version__)
 
 
--- pkg_publish.sh
+++ pkg_publish.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rm -rf dist/* &&  python setup.py bdist_wheel --universal && python3 setup.py sdist && twine upload dist/*
+rm -rf dist/* &&  python setup.py bdist_wheel --universal && python setup.py sdist && twine upload dist/*
+#rm -rf dist/* &&  python setup.py bdist_wheel --universal && python setup.py sdist && twine upload dist/*
